40eb909a2596aa3d6ae5ab4a209fc67be86e30f2
kamrulsaad <skamrul2680@gmail.com>
2023-02-14 13:52:58 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="App">
+      Hello from create vite app <br />
+      Hospital Management Website UNIECH
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
