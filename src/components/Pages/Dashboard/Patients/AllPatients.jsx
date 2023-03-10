cac90ec05a5e490f566ed38762c45bdaf364ce13
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-03-10 21:47:28 +0600

    added patient loading feature

--- src/components/Pages/Dashboard/Patients/AllPatients.jsx
+++ src/components/Pages/Dashboard/Patients/AllPatients.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import Spinner from "../../../Shared/Spinner";
 // import { useQuery } from 'react-query';
 
 const AllPatients = () => {
+  const [patients, setPatients] = useState({});
+  const [loading, setLoading] = useState(null)
+  // const url = `http://hms.uniech.com/api/v1/patient/all-patient`;
 
-
-    const [patients, setPatients] = useState({});
-    // const url = `http://hms.uniech.com/api/v1/patient/all-patient`;
-
-    // const { data: patients = [], refetch } = useQuery({
-    //     queryKey: ['patients'],
-    //     queryFn: async () => {
-    //         const res = await fetch(url);
-    //         const data = await res.json();
-    //         return data;
-    //     }
-    // })
+  // const { data: patients = [], refetch } = useQuery({
+  //     queryKey: ['patients'],
+  //     queryFn: async () => {
+  //         const res = await fetch(url);
+  //         const data = await res.json();
+  //         return data;
+  //     }
+  // })
 
   useEffect(() => {
-    // setLoading(true);
+    setLoading(true);
     fetch("http://hms.uniech.com/api/v1/patient/all-patient", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("LoginToken")}`,
@@ -25,60 +25,66 @@ const AllPatients = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        // setLoading(false);
+        setLoading(false);
         setPatients(data?.data);
       });
   }, []);
 
-//   if (loading) return <Spinner></Spinner>;
+    if (loading) return <Spinner></Spinner>;
 
   if (patients.length === 0)
-    return <h2 className="text-tahiti-red text-center mt-60 text-5xl ">No Patient Found</h2>;
+    return (
+      <h2 className="text-tahiti-red text-center mt-60 text-5xl ">
+        No Patient Found
+      </h2>
+    );
 
   return (
     <div>
-        <h1 className='text-5xl font-bold m-5 ml-10mt-10'>Patients</h1>
-        <button className='lg:ml-5 lg:mb-5 lg:mt-5 font-semibold p-1 rounded-sm btn-ghost bg-tahiti-red text-tahiti-white'>Add New</button>
-        <button className='lg:ml-5 lg:mb-5 lg:mt-5 font-semibold p-1 rounded-sm btn-ghost bg-tahiti-babyPink text-tahiti-black'>All Patients</button>
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Patient ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                {
-                        // patients.map((patient, i) => <tr key={ patient._id}>
-                        //     <th>{i + 1}</th>
-                        //     <td>{ patient?._id}</td>
-                        //     <td>{ patient?.firstName}</td>
-                        //     <td>{ patient?.lastName}</td>
-                        //     <td>{ patient?.email}</td>
-                        //     <td>{ patient?.phone}</td>
-                        //     <td><button>Details</button></td>
-                        //     {/* onClick={() => handleDeleteUser(user._id)} */}
-                        //     <td>
-                        //         {/* { patient?.role !== 'admin' &&  */}
-                        //         <button  className='btn btn-xs btn-danger'>Delete</button>
-                        //         {/* // } */}
-                        //         </td>
-                        // </tr>)
-                    }
-
-                </tbody>
-            </table>
-        </div>
-
-
-
+      <h1 className="text-5xl font-bold m-5 ml-10mt-10">Patients</h1>
+      <button className="lg:ml-5 lg:mb-5 lg:mt-5 font-semibold p-1 rounded-sm btn-ghost bg-tahiti-red text-tahiti-white">
+        Add New
+      </button>
+      <button className="lg:ml-5 lg:mb-5 lg:mt-5 font-semibold p-1 rounded-sm btn-ghost bg-tahiti-babyPink text-tahiti-black">
+        All Patients
+      </button>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Patient ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patients.length > 0 &&
+              patients.map((patient, i) => (
+                <tr key={patient._id}>
+                  <th>{i + 1}</th>
+                  <td>{patient?._id}</td>
+                  <td>{patient?.firstName}</td>
+                  <td>{patient?.lastName}</td>
+                  <td>{patient?.email}</td>
+                  <td>{patient?.phone}</td>
+                  <td>
+                    <button>Details</button>
+                  </td>
+                  {/* onClick={() => handleDeleteUser(user._id)} */}
+                  <td>
+                    {/* { patient?.role !== 'admin' &&  */}
+                    <button className="btn btn-xs btn-danger">Delete</button>
+                    {/* // } */}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
