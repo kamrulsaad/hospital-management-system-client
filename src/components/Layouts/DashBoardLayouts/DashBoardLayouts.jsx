0b6ce2756e180627d67779d9fad53f093dce7305
arbinzaman <526473+gb96@users.noreply.github.com>
2023-03-12 02:45:47 +0600

    Tried color change

--- src/components/Layouts/DashBoardLayouts/DashBoardLayouts.jsx
+++ src/components/Layouts/DashBoardLayouts/DashBoardLayouts.jsx
@@ -11,6 +11,11 @@ const DashBoardLayouts = () => {
         logOut()
         navigate("/login")
     }
+    // function changeColor() {
+
+    //     document.getElementById("color").setAttribute('fill', '#00CC99');
+    // id="color" onclick={changeColor}
+    //     }
 
     return (
 
@@ -88,10 +93,10 @@ const DashBoardLayouts = () => {
 
 
 
-                            <li ><Link to="/dashboard/signup"> <svg width="30" height="30" viewBox="0 0 56 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M34 38H22C9.75 38 0 47.875 0 60C0 62.25 1.75 64 4 64H52C54.125 64 56 62.25 56 60C56 47.875 46.125 38 34 38ZM6 58C7 50.125 13.75 44 22 44H34C42.125 44 48.875 50.125 49.875 58H6ZM28 32C36.75 32 44 24.875 44 16C44 7.25 36.75 0 28 0C19.125 0 12 7.25 12 16C12 24.875 19.125 32 28 32ZM28 6C33.5 6 38 10.5 38 16C38 21.625 33.5 26 28 26C22.375 26 18 21.625 18 16C18 10.5 22.375 6 28 6Z" fill="black" />
+                            <li  ><Link  to="/dashboard/signup"> <svg width="30" height="30" viewBox="0 0 56 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path  d="M34 38H22C9.75 38 0 47.875 0 60C0 62.25 1.75 64 4 64H52C54.125 64 56 62.25 56 60C56 47.875 46.125 38 34 38ZM6 58C7 50.125 13.75 44 22 44H34C42.125 44 48.875 50.125 49.875 58H6ZM28 32C36.75 32 44 24.875 44 16C44 7.25 36.75 0 28 0C19.125 0 12 7.25 12 16C12 24.875 19.125 32 28 32ZM28 6C33.5 6 38 10.5 38 16C38 21.625 33.5 26 28 26C22.375 26 18 21.625 18 16C18 10.5 22.375 6 28 6Z" fill="black" />
                             </svg>
-                                <span className='text-2xl font-semibold'>Add A New User</span></Link>
+                                <span  className='text-2xl font-semibold'>Add A New User</span></Link>
                             </li>
 
 
