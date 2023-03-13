f2fe9d6a5342476f779372fd584a453722155d32
kamrulsaad <skamrul2680@gmail.com>
2023-03-12 18:32:24 -0700

    removed comment

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -8,7 +8,6 @@ const Login = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
-    // console.log(from);
 
     const handleUserNamePassword = event => {
         event.preventDefault();
