5089861991b66a62d5362df393171f106aa98e58
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-03-25 14:12:11 +0600

    added preloader

--- src/components/Shared/Spinner.jsx
+++ src/components/Shared/Spinner.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="flex mt-20 min-h-[calc(100vh-200px)] items-center justify-center space-x-2 animate-bounce">
-      <div className="w-8 h-8 bg-tahiti-lightGreen rounded-full"></div>
-      <div className="w-8 h-8 bg-tahiti-darkGreen rounded-full"></div>
-      <div className="w-8 h-8 bg-tahiti-cyan rounded-full"></div>
+    <div className="flex mt-20 min-h-[calc(100vh-200px)] items-center justify-center space-x-2">
+      <img src="assets/preloader.gif" alt="Preloader" />
     </div>
   );
 };
