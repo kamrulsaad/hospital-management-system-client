18c07a7618c23caca6682e08b74b60c7ea54326c
arbinzaman <526473+gb96@users.noreply.github.com>
2023-03-31 23:35:16 +0600

    Marge conflic

--- src/components/Layouts/routes.jsx
+++ src/components/Layouts/routes.jsx
@@ -22,9 +22,9 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: (
-    //   <PrivateRoute>
+      <PrivateRoute>
         <DashBoardLayouts></DashBoardLayouts>
-    //   </PrivateRoute>
+      </PrivateRoute>
     ),
     children: [
       {
