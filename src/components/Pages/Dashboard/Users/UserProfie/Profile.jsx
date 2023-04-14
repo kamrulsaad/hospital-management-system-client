61bf3971d3ac1ded2459f7ae3c484a06aeb808bd
arbinzaman <526473+gb96@users.noreply.github.com>
2023-04-14 15:51:39 +0600

    Added Profile Picture feature

--- src/components/Pages/Dashboard/Users/UserProfie/Profile.jsx
+++ src/components/Pages/Dashboard/Users/UserProfie/Profile.jsx
@@ -8,6 +8,7 @@ const Profile = () => {
 
   const [userInfo, setUserInfo] = useState({});
   // fetching userInfo from backend
+  console.log(userInfo);
   useEffect(() => {
     fetch("https://hms-server.onrender.com/api/v1/user/user-info", {
       headers: {
@@ -23,18 +24,16 @@ const Profile = () => {
   return (
     <div className="flex">
       <div className="flex items-center justify-center w-1/4">
-        <svg
-          width="47"
-          height="51"
-          viewBox="0 0 56 64"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M34 38H22C9.75 38 0 47.875 0 60C0 62.25 1.75 64 4 64H52C54.125 64 56 62.25 56 60C56 47.875 46.125 38 34 38ZM6 58C7 50.125 13.75 44 22 44H34C42.125 44 48.875 50.125 49.875 58H6ZM28 32C36.75 32 44 24.875 44 16C44 7.25 36.75 0 28 0C19.125 0 12 7.25 12 16C12 24.875 19.125 32 28 32ZM28 6C33.5 6 38 10.5 38 16C38 21.625 33.5 26 28 26C22.375 26 18 21.625 18 16C18 10.5 22.375 6 28 6Z"
-            fill="white"
-          />
-        </svg>
+        {
+          <img src={userInfo?.data?.imageURL} alt="" className="w-16 rounded-full" /> || <svg  width="47" height="51" viewBox="0 0 56 64"  fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M34 38H22C9.75 38 0 47.875 0 60C0 62.25 1.75 64 4 64H52C54.125 64 56 62.25 56 60C56 47.875 46.125 38 34 38ZM6 58C7 50.125 13.75 44 22 44H34C42.125 44 48.875 50.125 49.875 58H6ZM28 32C36.75 32 44 24.875 44 16C44 7.25 36.75 0 28 0C19.125 0 12 7.25 12 16C12 24.875 19.125 32 28 32ZM28 6C33.5 6 38 10.5 38 16C38 21.625 33.5 26 28 26C22.375 26 18 21.625 18 16C18 10.5 22.375 6 28 6Z"
+              fill="white"
+            />
+          </svg>
+
+        }
+
       </div>
       <div>
         <h1 className="text-4xl font-semibold text-tahiti-white">
