b9562d1f9b7679bb251abd90864d0d9ce91fca5f
arbinzaman <526473+gb96@users.noreply.github.com>
2023-04-19 08:06:39 +0600

    fixed design bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
-    <div className='bg-tahiti-darkGreen' >
+    <div className='bg-tahiti-darkGreen ' >
       <RouterProvider router={router}></RouterProvider>
       <ToastContainer/>
     </div>
